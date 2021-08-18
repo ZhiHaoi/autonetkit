0d09da1124e7ef5ae12da2540b7df644197d9ed8
zhihao <zhangzhihao6@outlook.com>
2021-08-18 11:16:50 +0800

    u

--- ospf-49nodes-20210816/README.md
+++
@@ -1,2 +0,0 @@
-Uunet Topology
-49 nodes 
--- ospf-49nodes-20210816/mkFile.py
+++
@@ -1,6 +0,0 @@
-import os
-cwd = os.getcwd()
-for i in range(1,50):
-    os.mknod("/home/zzh/nsdi22/pybatfish/jupyter_notebooks/networks/ospf-test-49nodes/configs/r"+ str(i) + ".cfg") 
-    # fp = open("/home/zzh/nsdi22/pybatfish/jupyter_notebooks/networks/ospf-test-48nodes/configs/r%i.cfg", "w")
-    # fp.close()
